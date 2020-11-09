f785220b5557df8a8a50a130ca56fe4c65cdcf4f
c40753980 <nurbalaheybatov@std.iyte.edu.tr>
2020-11-09 15:48:42 +0000

    code of exp 2

--- lab4/example2.py
+++ lab4/example2.py
@@ -1,5 +1,5 @@
 year=int(input('Enter the year : '))
-if (year%100==0 and year%400==0):
+if (year%4==0 and year%100!=0 and year%400!=0) or (year%100==0 and year%400==0):
   print("Leap Year")
 else:
   print("Not Leap Year")
