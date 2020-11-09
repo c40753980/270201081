93f812eead91f1f75e9666f6d81d1d2d2ddd4f36
c40753980 <nurbalaheybatov@std.iyte.edu.tr>
2020-11-09 14:08:42 +0000

    code of exp 4

--- lab4/example4.py
+++ lab4/example4.py
@@ -1,6 +1,7 @@
-a=int(input("a : "))
-b=int(input("b : "))
-for i in a:
-  for i in b:
-    print(a*b)
+a = int(input("a : "))
+b = int(input("b : "))
+power = a
+for i in range(1,b):
+  power = power*a
+print(power)
 
