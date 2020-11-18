a8b5b99e42047aa435a4174b00d57e627378bf7a
c40753980 <nurbalaheybatov@std.iyte.edu.tr>
2020-11-18 13:28:27 +0000

    code of exp 3

--- lab5/example3.py
+++ lab5/example3.py
@@ -2,12 +2,12 @@ a=int(input("Enter number 1 : "))
 b=int(input("Enter number 2 : "))
 n=0
 
-while a>10 and b>10:
+while a>0 and b>0:
   if a%10==b%10:
     n+=1
   
-  a=a//10
-  b=b//10
+  a= a//10
+  b= b//10
 
-  print("Same digit number : "+ str(n+1) + "th")
+print(n)
  
