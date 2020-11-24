784e1fac28f3eac07701f8dac6715d57cbd914a1
c40753980 <nurbalaheybatov@std.iyte.edu.tr>
2020-11-24 15:12:31 +0000

    code of exp 4

--- lab6/example4.py
+++ lab6/example4.py
@@ -5,6 +5,7 @@ for x in n:
     if x not in unique_list:
       unique_list.append(x)
 
+#bubblesort
 for i in range(len(unique_list)):
         for j in range(len(unique_list)-1):
             if unique_list[j] > unique_list[j+1]:
