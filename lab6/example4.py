d9bc1639ee3785b0833b880dae98274dbeba7db4
c40753980 <nurbalaheybatov@std.iyte.edu.tr>
2020-11-24 15:11:33 +0000

    code of exp 4

---
+++ lab6/example4.py
@@ -0,0 +1,15 @@
+n = list(map(int,input("Enter your list : ").split(' ')))
+unique_list = []
+
+for x in n:
+    if x not in unique_list:
+      unique_list.append(x)
+
+for i in range(len(unique_list)):
+        for j in range(len(unique_list)-1):
+            if unique_list[j] > unique_list[j+1]:
+                c =  unique_list[j]
+                unique_list[j] = unique_list[j+1]
+                unique_list[j+1] = c
+
+print(unique_list)
