fe94f6078b167e94ca0f1add4d67a2ed4702a74c
c40753980 <nurbalaheybatov@std.iyte.edu.tr>
2020-11-24 01:16:53 +0000

    code of exp 5

---
+++ lab6/example2.py
@@ -0,0 +1,10 @@
+student=int(input("Enter the number of students : "))
+exam_grades=[]
+for student in exam_grades:
+  midterm1=int(input("Enter the grade of midterm 1 : "))
+  midterm2=int(input("Enter the grade of midterm 2 : "))
+  final=int(input("Enter the grade of final : "))
+
+  exam_grades.append([midterm1,midterm2,final])
+
+print(exam_grades)
---
+++ lab6/example5.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the size of Identity Matrix : "))
+for row in range(0,n):
+  for col in range(0,n):
+    if (row == col):
+      print("1 ", end=" ")
+    else:
+      print("0 ", end=" ")
+  print()
