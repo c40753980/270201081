369686e53b5f78031b5856e3566b0cb99b32f53e
c40753980 <nurbalaheybatov@std.iyte.edu.tr>
2020-12-14 09:37:58 +0000

    code of exp 3

--- lab8/example3.py
+++ lab8/example3.py
@@ -7,6 +7,19 @@ def get_list(b,e,N):
 
   return rl
 
+def get_overlap(L1,L2):
+
+  L3 = []
+
+  for i in L1:
+
+    if i in L2:
+
+      L3.append(i)
+
+  return L3
+
+
 def main():
 
   l1 = get_list(b=0,e=10,N=5)
@@ -14,6 +27,10 @@ def main():
 
   print(l1)
   print(l2)
+
+  l3 =get_overlap(l1,l2)
+
+  print(l3)
 
 
 main()
